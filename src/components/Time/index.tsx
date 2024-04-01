85eda6d67ba08c1bda1be59f39e651f7cf5d5259
ToluAdeoye <toluwaloope.adeoye@habaripay.com>
2024-04-01 13:35:12 +0100

    created Time component

---
+++ src/components/Time/index.tsx
@@ -0,0 +1,43 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import useInterval from "@/hooks/useInterval";
+import { Dispatch, SetStateAction } from "react";
+import { ClassNameValue, twMerge } from "tailwind-merge";
+
+type Props = {
+  hours: any;
+  minutes: any;
+  seconds: any;
+  timerStarted: boolean;
+  countDown?: number;
+  setCountDown: Dispatch<SetStateAction<number>>;
+  containerClass?: ClassNameValue;
+};
+
+const Time = ({
+  hours,
+  minutes,
+  seconds,
+  timerStarted,
+  setCountDown,
+  containerClass,
+}: Props) => {
+  useInterval(timerStarted, setCountDown);
+
+  return (
+    <div
+      className={twMerge(
+        "flex items-center select-none timer-font-size",
+        containerClass
+      )}
+    >
+      <p id="hour">{hours}</p>
+      <p className="column timer__item">:</p>
+      <p id="min">{minutes}</p>
+
+      <p className="column timer__item">:</p>
+      <p id="sec">{seconds}</p>
+    </div>
+  );
+};
+
+export default Time;
